de412f680ee87bd629301f258c9a86708be49b31
m-kac <m-kachalov@mail.ru>
2023-12-08 13:37:56 +0300

    fix js version

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRestController.java
@@ -1,14 +1,11 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.dto.UserDTO;
 import ru.kata.spring.boot_security.demo.exception.UserNotFoundException;
